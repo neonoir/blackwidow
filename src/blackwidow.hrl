75ded01aaf2ab87af7069c5f8814cdd794b935f9
Kamyar Navidan <kamyar.n@gmail.com>
2014-09-29 07:09:41 +0330

    add blackwidow header

--- src/blackwidow.hrl
+++ src/blackwidow.hrl
@@ -1,1 +1,6 @@
--record(bw_response, {url="" :: string(), status=200 :: integer(), headers=[] :: [{string(), string()}], body="" :: string()}).
+-record(bw_response, {
+	url="" :: string(), 
+	status=200 :: integer(), 
+	headers=[] :: [{string(), string()}], 
+	body="" :: string()
+	}).
