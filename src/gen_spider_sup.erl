a5f85f7f3680ac3ac54a3670c6e15a66077c9bfe
Kamyar Navidan <kamyar.n@gmail.com>
2014-09-29 07:09:01 +0330

    add gen spider supervisor

---
+++ src/gen_spider_sup.erl
@@ -0,0 +1,28 @@
+-module(gen_spider_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0,
+	 start_child/1
+	]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+start_child(Manager) ->
+    supervisor:start_child(?SERVER, [Manager]).
+    
+
+init([]) ->
+    Element = {gen_spider, {gen_spider, start_link, []},
+	       permanent, brutal_kill, worker, [gen_spider]},
+    Children = [Element],
+    RestartStrategy = {one_for_one, 0, 1},
+    {ok, RestartStrategy, Children}.
+    
+     
+	       
