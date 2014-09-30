99bb6a1b553cbb3a4c238e11c91257c3cfc7086c
Kamyar Navidan <kamyar.n@gmail.com>
2014-10-01 02:59:43 +0330

    add bw_spider_sup.erl - spider supervisor

---
+++ src/bw_spider_sup.erl
@@ -0,0 +1,24 @@
+-module(bw_spider_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link(SpiderModule, PipeLineModules) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [SpiderModule, PipeLineModules]).
+
+init([SpiderModule, PipeLineModules]) ->
+    GenSpiderSup = {gen_spider_sup, {gen_spider_sup, start_link, []},
+	       permanent, brutal_kill, supervisor, [gen_spider]},
+    SpiderManager = {bw_spider_manager, {bw_spider_manager, start_link, [SpiderModule, PipeLineModules]},
+		     permanent, brutal_kill, worker, [bw_spider_manager]},
+    Children = [GenSpiderSup, SpiderManager],
+    RestartStrategy = {one_for_all, 10, 5 * 3600},
+    {ok, RestartStrategy, Children}.
+    
+     
+	       
