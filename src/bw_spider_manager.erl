6516a14b62aca184486adb30e42c6e1a945c5390
Sina Samavati <sina.samv@gmail.com>
2014-10-25 00:09:50 +0330

    Make the code a little cleaner

--- src/bw_spider_manager.erl
+++ src/bw_spider_manager.erl
@@ -1,88 +1,106 @@
 -module(bw_spider_manager).
 
--export([start/2,
-	 start_link/2,
-	 init/2
-	]).
+-export([start/2]).
+-export([start_link/2]).
 
+-type worker() :: blackwidow:worker().
+-type workers() :: blackwidow:workers().
+-type url() :: blackwidow:url().
+-type urls() :: blackwidow:urls().
+
+%% -----------------------------------------------------------------------------
+%% API
+%% -----------------------------------------------------------------------------
+-spec start(module(), [module()]) -> {ok, pid()}.
 start(SpiderModule, PipeLineModules) ->
-    Pid = spawn(?MODULE, init, [SpiderModule, PipeLineModules]),
+    Pid = spawn(fun() -> init(SpiderModule, PipeLineModules) end),
     {ok, Pid}.
 
+-spec start_link(module(), [module()]) -> {ok, pid()}.
 start_link(SpiderModule, PipeLineModules) ->
-    Pid = spawn_link(?MODULE, init, [SpiderModule, PipeLineModules]),
+    Pid = spawn_link(fun() -> init(SpiderModule, PipeLineModules) end),
     {ok, Pid}.
 
--spec init(module(), [module()]) ->
-    ok.
+%% -----------------------------------------------------------------------------
+%% internal
+%% -----------------------------------------------------------------------------
+-spec init(module(), [module()]) -> ok.
 init(SpiderModule, PipeLineModules) ->
     NewUrls = gen_spider:start_urls(SpiderModule),
     MaxWorkers = gen_spider:max_workers(SpiderModule),
-    [ gen_spider_sup:start_child(SpiderModule, self()) || _ <- lists:seq(1, MaxWorkers) ],
-    IdleWorkers = get_workers(MaxWorkers),
-    {IdleWorkers2, BusyWorkers2, NewUrls2, OldUrls2} = assign_urls(IdleWorkers, [],  NewUrls, []),
-    manager_loop(IdleWorkers2, BusyWorkers2, NewUrls2, OldUrls2, PipeLineModules, MaxWorkers).
+    IdleWorkers = start_workers(SpiderModule, MaxWorkers),
+    {IdleWorkers1, BusyWorkers1, NewUrls1, OldUrls1} =
+        do_assign_urls(IdleWorkers, [], NewUrls, []),
+    loop(IdleWorkers1, BusyWorkers1, NewUrls1, OldUrls1,
+         PipeLineModules, MaxWorkers).
 
--spec get_workers(integer()) ->
-    blackwidow:workers().    
+-spec start_workers(module(), non_neg_integer()) -> workers().
+start_workers(SpiderModule, MaxWorkers) ->
+    [gen_spider_sup:start_child(SpiderModule, self()) ||
+        _ <- lists:seq(1, MaxWorkers)],
+    get_workers(MaxWorkers).
+
+-spec get_workers(non_neg_integer()) -> workers().
 get_workers(MaxWorkers) ->
     get_workers(MaxWorkers, []).
 
+-spec get_workers(non_neg_integer(), workers()) -> workers().
 get_workers(0, Workers) ->
     Workers;
 get_workers(MaxWorkers, Workers) ->
-    receive 
+    receive
 	{new_worker, NewWorker} ->
-	    get_workers(MaxWorkers -1, [NewWorker|Workers])
+	    get_workers(MaxWorkers - 1, [NewWorker|Workers])
     end.
 
--spec manager_loop(blackwidow:workers(), blackwidow:workers(), blackwidow:urls(), blackwidow:urls(), [module()], integer()) ->
-    ok.
-manager_loop(IdleWorkers, BusyWorkers, NewUrls, OldUrls, PipeLineModules, MaxWorkers) ->
-    case {length(IdleWorkers), NewUrls} of
-	{MaxWorkers, []} ->
-	    io:format("~n[CRAWL FINISHED]~n~nNumber of urls crawled: ~p ~n", [length(OldUrls)]),
-	    io:get_line("_____NEXT"),
-	    ok;
-	_ ->
-	    receive
-		{Worker, {{result, Result}, {urls, Urls}}} ->
-		    io:format("~n[ RESULT ] ~p~n", [Worker]),
-		    process_result(Result, PipeLineModules),
-		    Urls2 = remove_duplicate_urls(Urls, OldUrls),
-		    {IdleWorkers2, BusyWorkers2, NewUrls2, OldUrls2} = 
-			assign_urls([Worker | IdleWorkers], lists:delete(Worker, BusyWorkers),  lists:usort(NewUrls ++ Urls2), OldUrls),
-		    io:format("Urls\t Urls2\t NewUrls\t NewUrls2\t OldUrls\t OldUrls2 ~n"),
-		    io:format("~p~n", [{Urls, Urls2, NewUrls, NewUrls2, OldUrls, OldUrls2}]),
-		    manager_loop(IdleWorkers2, BusyWorkers2, NewUrls2, OldUrls2, PipeLineModules, MaxWorkers)
-	    end
+-spec loop(workers(), workers(), urls(), urls(), [module()], integer()) -> ok.
+loop(IdleWorkers, _, [], OldUrls, _, MaxWorkers)
+  when length(IdleWorkers) =:= MaxWorkers ->
+    io:format("~n[CRAWL FINISHED]~n~nNumber of urls crawled: ~p ~n", [length(OldUrls)]),
+    io:get_line("_____NEXT"),
+    ok;
+loop(IdleWorkers, BusyWorkers, NewUrls, OldUrls, PipeLineModules, MaxWorkers) ->
+    receive
+        {Worker, {{result, Result}, {urls, Urls}}} ->
+            io:format("~n[ RESULT ] ~p~n", [Worker]),
+            process_result(Result, PipeLineModules),
+            {IdleWorkers1, BusyWorkers1, NewUrls1, OldUrls1} =
+                assign_urls(Worker, IdleWorkers, BusyWorkers, Urls, NewUrls, OldUrls),
+            loop(IdleWorkers1, BusyWorkers1, NewUrls1, OldUrls1,
+                 PipeLineModules, MaxWorkers)
     end.
 
--spec assign_url(blackwidow:worker(), blackwidow:url()) ->
-    {blackwidow:worker(), {assign_url, blackwidow:url()}}.
+-spec assign_url(worker(), url()) -> {worker(), {assign_url, url()}}.
 assign_url(Worker, Url) ->
     io:format("~n[ ASSIGNED URL ~p TO ~p ]~n", [Url, Worker]),
     Worker ! {self(), {assign_url, Url}}.
 
--spec assign_urls(blackwidow:workers(), blackwidow:workers(), blackwidow:urls(), blackwidow:urls()) ->
-    {blackwidow:workers(), blackwidow:workers(), blackwidow:urls(), blackwidow:urls()}.
-assign_urls(IdleWorkers, BusyWorkers, [], OldUrls) ->
+-spec assign_urls(worker(), workers(), workers(), urls(), urls(), urls()) ->
+                         {workers(), workers(), urls(), urls()}.
+assign_urls(Worker, IdleWorkers, BusyWorkers, Urls, NewUrls, OldUrls) ->
+    IdleWorkers1 = [Worker | IdleWorkers],
+    BusyWorkers1 = lists:delete(Worker, BusyWorkers),
+    Urls1 = remove_duplicate_urls(Urls, OldUrls),
+    NewUrls1 = lists:usort(NewUrls ++ Urls1),
+    do_assign_urls(IdleWorkers1, BusyWorkers1, NewUrls1, OldUrls).
+
+-spec do_assign_urls(workers(), workers(), urls(), urls()) ->
+                            {workers(), workers(), urls(), urls()}.
+do_assign_urls(IdleWorkers, BusyWorkers, [], OldUrls) ->
     {IdleWorkers, BusyWorkers, [], OldUrls};
-assign_urls([], BusyWorkers, NewUrls, OldUrls) ->
+do_assign_urls([], BusyWorkers, NewUrls, OldUrls) ->
     {[], BusyWorkers, NewUrls, OldUrls};
-assign_urls([Worker|IdleWorkers], BusyWorkers, [Url|NewUrls], OldUrls) ->
+do_assign_urls([Worker|IdleWorkers], BusyWorkers, [Url|NewUrls], OldUrls) ->
     assign_url(Worker, Url),
-    assign_urls(IdleWorkers, [Worker | BusyWorkers], NewUrls, [ Url | OldUrls]).
+    do_assign_urls(IdleWorkers, [Worker | BusyWorkers], NewUrls, [Url | OldUrls]).
 
--spec remove_duplicate_urls(blackwidow:urls(), blackwidow:urls()) ->
-    [blackwidow:urls()].
+-spec remove_duplicate_urls(urls(), urls()) -> urls().
 remove_duplicate_urls(NewUrls, OldUrls) ->
-    [ Url || Url <- NewUrls, not lists:member(Url, OldUrls) ].
+    [Url || Url <- NewUrls, not lists:member(Url, OldUrls)].
 
+-spec process_result(term(), [module()]) -> ok.
+process_result(ok, _) ->
+    ok;
 process_result(Result, PipeLineModules) ->
-    case Result of
-	ok ->
-	    ok;
-	_ ->
-	    spawn(gen_spider_pipeline, handle_process_result, [Result, PipeLineModules])
-    end.
+    spawn(gen_spider_pipeline, handle_process_result, [Result, PipeLineModules]),
+    ok.
